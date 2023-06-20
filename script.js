c3bba3319212ffa57e5844c4745326c365123a9a
Mohit Kamdi <130160983+Mohit27071998@users.noreply.github.com>
2023-06-20 17:27:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function updateStatus() {
+  var status = document.getElementById("status");
+  var newHeading = document.createElement("h1");
+  newHeading.textContent = "Entered Metaverse";
+  status.parentNode.replaceChild(newHeading, status);
+}
